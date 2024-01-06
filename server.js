9d3f5f234fcf87528791c097caf7ea9ff4fdffa3
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2024-01-06 15:13:19 +0530

    init server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(    express.json()  );
+
+const PORT = 2101;
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
